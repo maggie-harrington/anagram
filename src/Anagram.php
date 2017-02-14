2bd914144055ba2acd486f894abbc12c70ec4b60
David Quisenberry and Maggie Harrington <maggie@>
2017-02-14 08:45:15 -0800

    create initial files

---
+++ src/Anagram.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Anagram {
+        function createAnagrams($string)
+        {
+            
+        }
+
+
+    }
+
+
+ ?>
---
+++ tests/AnagramTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once "src/Anagram.php";
+class AnagramTest extends PHPUnit_Framework_TestCase
+{
+    function test_checkAnagram_false()
+    {
+        $test_Anagram = new Anagram;
+
+        
+    }
+}
+?>
